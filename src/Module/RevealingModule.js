7498d72571a1ec43a81ad69ea2df3c93240400e6
reintrance <reintrance@gmail.com>
2016-09-04 23:03:44 +0300

    Module patterns

--- src/Module/RevealingModule.js
+++ src/Module/RevealingModule.js
@@ -1,1 +1,22 @@
-RevealingModule.js
+/*
+ * In revealing pattern you would simply define all of
+ * your functions and variables in the private scope and return an anonymous object at
+ * the end of the module along with pointers to both the private variables and functions
+ * you wished to reveal as public.
+ */
+var module = (function {
+    var name = 'Bob';
+
+    function getName () {
+        return name;
+    }
+
+    function setName (newName) {
+        name = newName;
+    }
+
+    return {
+        get: getName,
+        set: setName
+    };
+})();
--- src/Module/SimpleModule.js
+++ src/Module/SimpleModule.js
@@ -1,17 +1,19 @@
 // Example of simple module patter implementation and use with support of private members
 
 var simpleModule = (function () {
-	var counter = 0;
+    var counter = 0;
 
-	return {
-		incrementCounter: function () {
-			return counter++;
-		},
-		resetCounter: function () {
-			console.log('Counter was reset');
-			counter = 0;
-		}
-	};
+    // Other private members
+    return {
+        // Public members
+        incrementCounter: function () {
+            return counter++;
+        },
+        resetCounter: function () {
+            console.log('Counter was reset');
+            counter = 0;
+        }
+    };
 })();
 
 simpleModule.incrementCounter();

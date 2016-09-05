6a2e2aaa2df684b2b2c40ca10cdb623b5519537c
reintrance <reintrance@gmail.com>
2016-09-05 15:43:09 +0300

    Simple observer design pattern implementation

---
+++ src/Observer/SimpleObserver.js
@@ -0,0 +1,52 @@
+var pubsub = {};
+
+(function (q) {
+    var topics = {},
+        subUid = -1;
+
+    q.publish = function (topic, args) {
+        if (!topics[topic]) {
+            return false;
+        }
+
+        var subscribers = topics[topic],
+            len = subscribers ? subscribers.length : 0;
+
+        while (len--) {
+            subscribers[len].func(topic, args);
+        }
+
+        return this;
+    };
+
+    q.subscribe = function (topic, func) {
+        if (!topics[topic]) {
+            topics[topic] = [];
+        }
+
+        var token = (++subUid).toString();
+
+        topics[topic].push({
+            token: token,
+            func: func
+        });
+
+        return token;
+    }; 
+
+    q.unsubscribe = function (token) {
+        for (var m in topics) {
+            if (topics[m]) {
+                for (var i = 0, len = topics[m].length; i < len; i++) {
+                    if (topics[m][i].token === token) {
+                        topics.splice(i, 1);
+                        return token;
+                    }
+                }
+            }
+        }
+
+        return this;
+    };
+
+})(pubsub);
